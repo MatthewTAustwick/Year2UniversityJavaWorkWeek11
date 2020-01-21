eaa329f88c786e94c74e7a07e5b7d5c151269156
MatthewTAustwick <57094484+MatthewTAustwick@users.noreply.github.com>
2020-01-21 17:00:56 +0000

    Update TopologicalSortClass.java

--- src/graph/TopologicalSortClass.java
+++ src/graph/TopologicalSortClass.java
@@ -8,10 +8,30 @@ public class TopologicalSortClass<T> extends AdjacencyGraph<T> implements Topolo
     private int noOfNodes;
     private int noOfEdges;
     private Hashtable<T,Set<T>> adjacencyList = new Hashtable<T, Set<T>>();
+    private HashMap<T, Integer> nodeReferenceCount = new HashMap<T, Integer>(); //Creates a hashmap to store the reference count of all the nodes
     public List<T> traversalList = new ArrayList<>();
 
-    public void getSuccessors(T node) throws GraphError{
+    public void createReferenceCounting() throws GraphError{
+        for (T givenNode: getNodes()){
+            nodeReferenceCount.put(givenNode, 0); //Get the neighbours for the nodes and put them in the hashmap
+        }
+        for (T givenNode: getNodes()){
+            for (T neighbourNode: getNeighbours(givenNode)){
+                increaseReferenceCount(neighbourNode); //Increase the reference count of the node if it has any successors.
+            }
+        }
     }
+
+    private void increaseReferenceCount(T neighbourNode){
+        Integer nodeCount = nodeReferenceCount.get(neighbourNode);
+            if (nodeCount == null){
+                nodeCount = 1;
+            }
+            else {
+                nodeCount ++; //Increment the count if the count is not already 0
+            }
+            nodeReferenceCount.put(neighbourNode, nodeCount);
+        }
 
     public Set<T> getNeighbours(T node) throws GraphError{
         Set<T> a = new HashSet<T>(); //Create a new hashset to store the nodes in
@@ -31,5 +51,19 @@ public class TopologicalSortClass<T> extends AdjacencyGraph<T> implements Topolo
 
     public int getNumberOfNodes (){ //Return the number of nodes
         return noOfNodes;
+    }
+
+    public static void main(String[] args) throws GraphError {
+        TopologicalSortClass<Integer> tsg = new TopologicalSortClass<Integer>();
+        for (int i = 0; i < 6; i++){
+            tsg.add(i);
+        }
+        tsg.add(0,1);
+        tsg.add(1,2);
+        tsg.add(2,3);
+        tsg.add(0,2);
+        tsg.add(4,5);
+        tsg.add(3,5);
+
     }
 }
